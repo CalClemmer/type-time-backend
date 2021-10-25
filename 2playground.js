d43008fac5db4c641e0de97c76448a80ae43465c
CalClemmer <cal1296@yahoo.com>
2021-10-25 09:18:49 -0700

    Provides bee data

--- 2playground.js
+++ 2playground.js
@@ -1,5 +1,7 @@
 const { Text } = require("./models"); // import text
 
+const beeMovieJson = require("./models/text_data/beeMovie.json");
+
 // Text.remove({}, function (err) {
 //   console.log("Text Database Dropped");
 // });
@@ -8,5 +10,14 @@ async function findBee() {
   let bee = await Text.find({});
   console.log(bee);
 }
+
+// Text.insertMany(beeMovieJson, function (err, result) {
+//   if (err) {
+//     console.log(err);
+//     console.log("It didn't insert...");
+//   } else {
+//     console.log("We got the bee, we got the bee (successfully inserted)");
+//   }
+// });
 
 findBee();
